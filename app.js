006f022675ce59047632c6293ad48aed4fb7b80e
freddtastic <fredrik.pga@gmail.com>
2015-07-30 19:07:26 +0200

    tests

--- app.js
+++ app.js
@@ -79,6 +79,7 @@
       // Clears screen
       console.log(messages.CLEARSCREEN);
       console.log(messages.TIMESTAMP + messages.TAB + clc.bold("Väder: ") + clc.magenta(Data["WeatherData"]["today"][0]["t"]) + " °C | " + clc.magenta(Data["WeatherData"]["today"][0]["ws"]) + " m/s");
+      console.log(clc.green("---------------------------"));
 
       if(t_diffSecs == 0){ console.log("[Uppdatering -> " + t_diffMins + ":00]"); }
       else if(t_diffSecs < 10){ console.log("[Uppdatering -> " + t_diffMins + ":0" + t_diffSecs + "]"); }
