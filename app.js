80ab7d2f99129d7a1c75276d3ada237382ae44a6
freddtastic <fredrik.pga@gmail.com>
2015-07-30 17:53:42 +0200

    fixed weather function

--- app.js
+++ app.js
@@ -68,7 +68,7 @@
     if(Data === undefined || Data == null){
       
     }else{
-      console.log(Data["BusData"]);
+      console.log(Data);
     }
 
     // Formatting coming soon
@@ -102,7 +102,6 @@
 
         var buffer = "";
         var lines = [];
-        var station = "";
         var bus_0 = [];
         var bus_1 = [];
 
@@ -128,6 +127,8 @@
             var date_1 = new Date(bus_1[0].JourneyDateTime);
 
             // Put all our data at one place
+            Bus.bus_0_line = bus_0;
+            Bus.bus_1_line = bus_1;
             Bus.bus_0_station = bus_0[0].Towards[0];
             Bus.bus_1_station = bus_1[0].Towards[0];
             Bus.bus_0_msg = "[-> " + bus_0[0].Towards + "]: " + date_0.getUTCHours() + ":" + date_0.getUTCMinutes();
@@ -159,6 +160,8 @@
 
   function getWeather(latLonS){
 
+    var Weather = {};
+
     var deferred = Q.defer(),
     completedRequests = 0;
 
@@ -168,10 +171,9 @@
       http.get("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/" + latLon.lat + "/lon/" + latLon.lon + "/data.json", function(res) {
 
         var days = [];
+        var list = [];
+        var current = [];
         var buffer = "";
-        var msg_c = "";
-        var msg_t = "";
-        var msg_r = "";
 
         res.on("data", function (chunk) {
           buffer += chunk; 
@@ -180,65 +182,26 @@
         res.on("end", function (e) {
 
           var weather = JSON.parse(buffer);
-          var x = 0;
+          var days = weather.timeseries
 
-          days = weather.timeseries;
+          for(var i = 0; i < days.length; i++){       
 
-          for(var i = 0; i < days.length; i++){
+            var x = new Date(days[i].validTime);
 
-            var temp = new Date(days[i].validTime);
+            if(today.getDay() == x.getDay() && today.getHours() < x.getUTCHours()){
+              current.push({t:days[i].t, T:days[i].validTime, pit:days[i].pit});
+            }
 
-            if(today.getDay() == temp.getDay()){
-
-                var hours = temp.getHours();
-
-                if(hours == 0 || hours == 2){
-                  hours = 24;
-                }
-
-                if(today.getHours() <= hours){             
-
-                  if(x == 0){
-                    msg_c += "  | " + clc.underline(hours) + ":" + clc.underline(temp.getMinutes()) + " | ";
-                    msg_t += "  | ";
-                    msg_r += "  | ";
-                  }else if(i == days.length -1){
-                    msg_c += hours + ":" + temp.getMinutes() + " |";
-                  }else{
-                    msg_c += hours + ":" + temp.getMinutes() + " | ";
-                  }
-
-                  if(days[i].t > 20){
-                    msg_t += clc.red(days[i].t.toFixed(1));
-                  } else if(days[i].t > 15){
-                    msg_t += clc.green(days[i].t.toFixed(1));
-                  } else if(days[i].t > 10){
-                    msg_t += clc.yellow(days[i].t.toFixed(1));
-                  } else if(days[i].t > 0){
-                    msg_t += clc.cyan(days[i].t.toFixed(1));
-                  } else if(days[i].t <= 0){
-                    msg_t += clc.blue(days[i].t.toFixed(1));
-                  }
-
-                  msg_r += days[i].pit.toFixed(2);
-
-                  msg_t += " | ";
-                  msg_r += " | ";
-
-                  x++;
-
-                }
-
-              }
+            list.push({t:days[i].t, T:days[i].validTime, pit:days[i].pit});
 
           }
 
-          //messages.push(clc.bold("Väder"));
-          //messages.push("");
-          //messages.push(msg_c);
-          //messages.push(msg_t);
-          //messages.push(msg_r);
-          //messages.push("");
+          // Put all our data at one place
+          Weather.list = list;
+          Weather.today = current;
+
+          // Finally store it!
+          Data["WeatherData"] = Weather;
 
           completedRequests++;
 
